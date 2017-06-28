5cabdc7b4c36591096518aa82c2eff9ca21c15b1
nikitinskaya <nikitinskaya.v@gmail.com>
2017-06-28 20:58:15 +0300

    Wrap everything in one function
    This is preferred, though I find the code less clear than in the previous version

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
-$(document).ready(function() {
-  newQuote();
-  $('#getQuote').on("click", newQuote);
-});
+$(function() {
 
-function newQuote() {
-  getQuote();
-  changeColor();
-};
+  function getQuote() {
+    $.ajax({
+      url: "https://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      cache: false,
+      success: function(data) {
+        $("#quoteText").html(data.quoteText);
+        if (data.quoteAuthor) {
+          $("#quoteAuthor").html("– " + data.quoteAuthor);
+        } else {
+            $("#quoteAuthor").html("");
+        }
+        $("#tweetQuote").attr(
+          "href",
+          "https://twitter.com/intent/tweet?text=" +
+            data.quoteText +
+            "(Quote by " +
+            data.quoteAuthor +
+            ")"
+          );
+        }
+      });
+    };
 
-function getQuote() {
-  $.ajax({
-    url: "https://api.forismatic.com/api/1.0/?",
-    dataType: "jsonp",
-    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-    cache: false,
-    success: function(data) {
-      $("#quoteText").html(data.quoteText);
-      if (data.quoteAuthor) {
-        $("#quoteAuthor").html("– " + data.quoteAuthor);
-      } else {
-          $("#quoteAuthor").html("");
-      }
-      $("#tweetQuote").attr(
-        "href",
-        "https://twitter.com/intent/tweet?text=" +
-          data.quoteText +
-          "(Quote by " +
-          data.quoteAuthor +
-          ")"
-        );
-      }
-    });
+  var colors = [
+    "#997777",
+    "#838a85",
+    "#8c7884",
+    "#779970",
+    "#9fb1a2",
+    "#748f8b",
+    "#6d9cb3"
+  ];
+
+  function changeColor() {
+    var color = Math.floor(Math.random() * colors.length);
+    $(".decorate").css("color", colors[color]);
+    $(".decorate").css("border-color", colors[color]);
   };
 
-var colors = [
-  "#997777",
-  "#838a85",
-  "#8c7884",
-  "#779970",
-  "#9fb1a2",
-  "#748f8b",
-  "#6d9cb3"
-];
+  function newQuote() {
+    getQuote();
+    changeColor();
+  };
 
-function changeColor() {
-  var color = Math.floor(Math.random() * colors.length);
-  $(".decorate").css("color", colors[color]);
-  $(".decorate").css("border-color", colors[color]);
-};
+  newQuote();
+
+  $('#getQuote').click(newQuote);
+
+});
